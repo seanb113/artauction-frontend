7e971d54963acc4a22e1fda89ff653c98f3459ad
Sean Beach <sean.beach1@yahoo.com>
2020-09-09 23:05:48 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+
+import React, { Component } from 'react'
+import Landing from './Components/landing'
+import painting from './Components/painting'
+
+class App extends Component {
+  state = {
+    paintings: [],
+    displayed: []
+  }
+
+  getpaintings(){
+    fetch('http://localhost:3000/crud')
+      .then(response => response.json())
+      .then(paintings => this.setState({
+        paintings: paintings,
+        displayed: paintings}))
+      .catch(err => console.log(err))
+  }
+
+  selectPainting = (p) => {
+    debugger
+    let selected = [this.state.paintings.find(paint => paint.id === p.id)]
+    this.setState({
+      displayed: selected
+    })
+  }
+
+  goBack = () => {
+    this.setState({
+      displayed: this.state.paintings
+    })
+  }
+
+  likePainting = (p) => {
+    debugger
+    let count = p.likes != null ? p.likes + 1 : 1
+      fetch('http://localhost:3000/crud', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        id: p.id,
+        likes: count
+      })
+    })
+      .then(response => response.json())
+      .then(item => {
+        this.updateCount(p, count)
+      })
+      .catch(err => console.log(err))
+    }
+
+  updateCount = (p, c) => {
+    debugger
+    // let painting = this.state.paintings.find(paint => paint.id === p.id)
+    // painting.votes = c
+    let updatedPaintings = this.changeVotes(p.id, c)
+    this.setState({
+      displayed: updatedPaintings
+    })
+  }
+
+changeVotes = ( id, likes ) => {
+   for (var i in this.state.displayed) {
+     if (this.state.displayed[i].id === id) {
+        this.state.displayed[i].likes = likes;
+        break;
+     }
+   }
+   return this.state.displayed
+}
+
+
+  componentDidMount(){
+    this.getpaintings()
+  }
+
+render(){
+    return (
+
+      <div>
+      <Landing paintings={this.state.displayed} selectPainting={this.selectPainting} likePainting={this.likePainting}/>
+      {this.state.displayed.length === 1 ? <button onClick={this.goBack}>Go back to all paintings.</button> : null}
+      </div>
+    )
+}
+}
+
+export default App
---
+++ src/Components/landing.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Landing = props => {
+    console.log(props)
+    return(
+    <div class="grid-container">
+    {props.paintings.map(painting =>
+    <div class="grid-item">
+    <div>{painting.name}</div>
+    <img src={painting.url} onClick={() => props.selectPainting(painting)}></img>
+    <div>{painting.artist}, {painting.painted_in}.</div>
+    <div>{painting.likes != null ? painting.likes : '0'}</div>
+    <button onClick={() => props.likePainting(painting)}>Like</button>
+    </div>
+    )}
+    </div>
+    )
+}
+
+export default Landing
